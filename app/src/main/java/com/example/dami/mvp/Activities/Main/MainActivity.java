2986287778de33bf440f15767166fdc482114c9d
Damian Pasek <damian.pasek95@gmail.com>
2018-06-25 23:43:35 +0200

    new itemLayout and added sh_cycle

--- app/src/main/java/com/example/dami/mvp/Activities/Main/MainActivity.java
+++ app/src/main/java/com/example/dami/mvp/Activities/Main/MainActivity.java
@@ -6,11 +6,15 @@ import android.os.Bundle;
 import com.example.dami.mvp.R;
 
 public class MainActivity extends AppCompatActivity implements  MainContract.View {
+    private  MainPresenter mPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Init
+        mPresenter = new MainPresenter();
+
     }
 
     @Override
